0caa95acfac56436cda49ea40c229afab6758264
mdfaizan973 <faizan.md9735@gmail.com>
2024-02-10 22:12:56 +0530

    able to complete the mid navbar

--- kbcollagebermo/src/Components/NavBars.jsx/Navbar.jsx
+++ kbcollagebermo/src/Components/NavBars.jsx/Navbar.jsx
@@ -25,22 +25,24 @@ export default function Navbar() {
             </div>
           </div>
 
-          {/* <div className="content">
-            <div className="image_logos">
-              <img
-                src="https://kbcollegebermo.ac.in/images/bbmku.png"
-                alt="logo"
-              />
-            </div>
-            <div className="image_logos">
-              {" "}
-              <img
-                src="https://kbcollegebermo.ac.in/images/bbmku.png"
-                alt="logo"
-              />
-            </div>
-          </div> */}
-
+          <div className="menu_button">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#000000"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-menu"
+            >
+              <line x1="3" y1="12" x2="21" y2="12"></line>
+              <line x1="3" y1="6" x2="21" y2="6"></line>
+              <line x1="3" y1="18" x2="21" y2="18"></line>
+            </svg>
+          </div>
           <div className="content_links">
             <p href="#">Home</p>
             <p href="#">Departments</p>
@@ -98,26 +100,43 @@ const Nav = styled.div`
   }
   nav .content_links {
     display: flex;
+    align-items: center;
     width: 55%;
     justify-content: space-between;
   }
   nav .content_links p {
-    font-weight: bold;
     font-weight: 600;
-    font-size: 16px;
-    color: #303f92;
+    font-size: 18px;
+    color: #0a1347;
     padding: 18px 15px;
     cursor: pointer;
+  }
+  nav .content_links p:hover {
+    border: 1px solid #0a1347;
+    padding: 4px 12px;
+  }
+  .menu_button {
+    display: none;
+    cursor: pointer;
+  }
+  .menu_button:hover {
+    background-color: rgb(195, 194, 194);
   }
 
   @media screen and (min-width: 100px) and (max-width: 450px) {
     nav .content_links {
       display: none;
     }
+    .menu_button {
+      display: block;
+    }
   }
   @media screen and (min-width: 451px) and (max-width: 999px) {
     nav .content_links {
       display: none;
+    }
+    .menu_button {
+      display: block;
     }
   }
 `;
