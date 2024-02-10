73cccaa82c75b764c789ef6907264f8fb84fb4b5
mdfaizan973 <faizan.md9735@gmail.com>
2024-02-10 20:13:18 +0530

    able to make top nav

--- kbcollagebermo/src/Components/NavBars.jsx/Navbar.jsx
+++ kbcollagebermo/src/Components/NavBars.jsx/Navbar.jsx
@@ -1,3 +1,17 @@
+import TobNav from "./TobNav";
+import styled from "styled-components";
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <TobNav />
+      <P>There will be Main_Navbar</P>
+    </div>
+  );
 }
+
+const P = styled.p`
+  text-align: center;
+  color: red;
+  font-size: 24px;
+  font-weight: bold;
+`;
--- kbcollagebermo/src/Components/NavBars.jsx/TobNav.jsx
+++ kbcollagebermo/src/Components/NavBars.jsx/TobNav.jsx
@@ -1,3 +1,81 @@
+import { BsFacebook, BsTwitter, BsLinkedin, BsInstagram } from "react-icons/bs";
+
+import styled from "styled-components";
 export default function TobNav() {
-  return <div>TobNav</div>;
+  return (
+    <Nav>
+      <nav className="main_top_nav">
+        <div className="email_phone">
+          <p className="collage_name">
+            -- Official Website of K.B. College, Bermo
+          </p>
+          <p className="email"> Email: principalbermo@gmail.com</p>
+          <p className="phome">Call +91 8092129391</p>
+        </div>
+        <div className="social_media_icons">
+          <p className="fa">
+            <BsFacebook />
+          </p>
+          <p className="tw">
+            <BsTwitter />
+          </p>
+          <p className="li">
+            <BsLinkedin />
+          </p>
+          <p className="in">
+            <BsInstagram />
+          </p>
+          <p className="register">Register</p>
+        </div>
+      </nav>
+    </Nav>
+  );
 }
+const Nav = styled.nav`
+  background-color: red;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  nav {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    padding: 8px;
+    justify-content: space-around;
+  }
+
+  nav div p {
+    color: white;
+    font-weight: bold;
+    cursor: pointer;
+  }
+
+  .email_phone {
+    display: flex;
+    flex-wrap: wrap;
+    width: 50%;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .collage_name {
+    color: rgb(109, 109, 109);
+  }
+  .social_media_icons {
+    display: flex;
+    flex-wrap: wrap;
+    width: 18%;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .social_media_icons .register {
+    border: 1px solid white;
+    padding: 4px 12px;
+  }
+  @media screen and (min-width: 100px) and (max-width: 450px) {
+    .email_phone {
+      display: none;
+    }
+    .social_media_icons {
+      width: 90%;
+      margin: auto;
+    }
+  }
+`;
