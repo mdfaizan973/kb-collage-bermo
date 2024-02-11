fd8e7577bb7d8c72d8ff84161bb3e4670bd804aa
mdfaizan973 <faizan.md9735@gmail.com>
2024-02-11 18:04:53 +0530

    able otmake footer design

--- kbcollagebermo/src/App.jsx
+++ kbcollagebermo/src/App.jsx
@@ -1,14 +1,14 @@
 // import AllRoutes from "./Components/AllRoutes";
 import Navbar from "./Components/NavBars.jsx/Navbar";
 import Page_Landing from "./Pages/Home_Page/Page_Landing";
-// import Footer from "./Components/NavBars.jsx/Footer";
+import Footer from "./Components/NavBars.jsx/Footer";
 export default function App() {
   return (
     <div>
       <Navbar />
       {/* <AllRoutes /> */}
       <Page_Landing />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
--- kbcollagebermo/src/Components/NavBars.jsx/Footer.jsx
+++ kbcollagebermo/src/Components/NavBars.jsx/Footer.jsx
@@ -1,3 +1,93 @@
+import styled from "styled-components";
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <DIV>
+      <footer>
+        <div className="college_logo">
+          <img
+            src="https://kbcollegebermo.ac.in/images/logo-white.png"
+            alt="logo"
+          />
+        </div>
+        <div className="college_links1">2</div>
+        <div className="college_links2">3</div>
+        <div className="college_links3">4</div>
+      </footer>
+      <div className="footer">
+        <div className="copyright">
+          <p>
+            ©Copyright 2024 K.B. College, Bermo All Rights Reserved <br />
+            <span>
+              Design & Developed By -
+              <span className="developer">
+                <i>Md Faizan</i>
+              </span>
+            </span>
+          </p>
+        </div>
+      </div>
+    </DIV>
+  );
 }
+
+const DIV = styled.div`
+  footer {
+    height: 50vh;
+    width: 99%;
+    margin: auto;
+    color: white;
+    background-color: #ff7900;
+    border-radius: 50px 50px 0 0;
+    display: flex;
+    justify-content: space-around;
+  }
+  .college_logo {
+    border: 1px solid white;
+    width: 23%;
+    text-align: center;
+    padding: 10px;
+  }
+  .college_links1 {
+    border: 1px solid white;
+    width: 23%;
+    text-align: center;
+  }
+  .college_links2 {
+    border: 1px solid white;
+    width: 23%;
+    text-align: center;
+  }
+  .college_links3 {
+    border: 1px solid white;
+    width: 23%;
+    text-align: center;
+  }
+  .copyright {
+    width: 99%;
+    height: 15vh;
+    margin: auto;
+    color: white;
+    background-color: #fd8821;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-size: 18px;
+  }
+  .developer {
+    font-weight: 700;
+    color: red;
+  }
+  @media screen and (min-width: 100px) and (max-width: 450px) {
+    footer {
+      display: flex;
+      flex-direction: column;
+    }
+    footer .college_logo,
+    .college_links1,
+    .college_links2,
+    .college_links3 {
+      width: 100%;
+    }
+  }
+`;
